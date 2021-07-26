d527c82783241abecc8b57034e710c20cf6d4df3
Thonipho <thonipho@icloud.com>
2021-07-26 13:29:28 +0200

    Code College

---
+++ Java11Classwork/src/main/java/Chap02/SayHello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chap02;
+
+/**
+ *
+ * @author User
+ */
+public class SayHello {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        
+        String s3 = s1 + "" + s2;
+        
+        double d1 = 3000000;
+        double d2 = 23000200;
+        
+        double d3 = d1 + d2;
+        System.out.println(d3);
+                
+    }
+}
