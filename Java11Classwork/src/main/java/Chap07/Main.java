09e70b66bdb66d13a5a8ed17c1849e0a830a7448
Thonipho <thonipho@icloud.com>
2021-07-27 13:14:17 +0200

    Code College

---
+++ Java11Classwork/src/main/java/Chap07/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chap07;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer01 = new Customer("John Wick", 23, "172 Riche st", 9399, 2929);
+        customer01.displayCustomer();
+    }
+}
