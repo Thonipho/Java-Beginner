7d531a030d038522efa00782d69ef71cba90b3b1
Thonipho <thonipho@icloud.com>
2021-07-29 14:58:54 +0200

    Code College

--- Java11Classwork/src/main/java/sg1/Chap07/Main.java
+++ Java11Classwork/src/main/java/sg1/Chap07/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Chap07;
+package sg1.Chap07;
 
 /**
  *
---
+++ Java11Classwork/src/main/java/sg2/lesson12/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg2.lesson12;
+
+/**
+ *
+ * @author User
+ */
+public class main {
+    public static void main(String[] args) {
+        shirt shirt01 = new shirt ('M', "Hawain", 'R', 15, "short sleeve",'S',55.5);
+        shirt01.display();
+    }
+}
