aaa05e1608b3590dfd7cece75673899293d54953
Thonipho <thonipho@icloud.com>
2021-08-07 15:08:54 +0200

    Code College

--- Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterTwo/Casting.java
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterTwo/Casting.java
@@ -15,7 +15,30 @@ public class Casting {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Demonstrate casting
+        
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+        
+        x = 10.0;
+        y = 3.0;
+        
+        i = (int) (x/y); //cast double to int
+        System.out.println("Integer outcome of x/y: " +i);
+        
+        i = 100;
+        b = (byte) i;
+        System.out.println("Value of b: "+b);
+        
+        i = 257;
+        b = (byte) i;
+        System.out.println("Value of b: "+b);
+        
+        b = 88;
+        ch = (char) b;
+        System.out.println("ch: "+ch);
     }
     
 }
