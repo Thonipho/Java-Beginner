05020d808b589f3b37cf608451b8a64152b82692
Thonipho <thonipho@icloud.com>
2021-08-08 13:02:36 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterThree/GuessGame.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterThree;
+import java.io.IOException;
+/**
+ *
+ * @author User
+ */
+public class GuessGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+        // Guess the letter game
+        
+        throws java.io.IOException {
+        
+        char ch, awnser = 'K';
+        
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.print("Can you guess it: ");
+        
+        ch = (char) System.in.read(); // read a char from the keyboard
+        
+        if(ch == awnser) System.out.println("**Right**");
+        else System.out.println("...Sorry, youre wrong");
+    }
+    
+    
+}
