82f8357d80019d7814816554c765ea8242d5b5d6
Thonipho <thonipho@icloud.com>
2021-08-08 13:44:15 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterThree/Help.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterThree;
+
+/**
+ *
+ * @author User
+ */
+public class Help {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws java.io.IOException {
+        // a simple help system
+        
+        char choice;
+        
+        System.out.println("Help on: ");
+        System.out.println("\t1. if ");
+        System.out.println("\t2. switch ");
+        System.out.println("Choose one: ");
+        choice = (char) System.in.read();
+        
+        System.out.println("\n");
+        
+        switch(choice) {
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition) statement;");
+                System.out.println("else statement;");
+                break;
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch(expression) {");
+                System.out.println("\tcase constant:");
+                System.out.println("\t\tstatement sequence");
+                System.out.println("\t\tbreak");
+                System.out.println("\t// ...");
+                System.out.println("}");
+                break;
+            default:
+                System.out.println("Selection not found");
+        }
+    }
+    
+}
---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterThree/SwitchDemo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterThree;
+
+/**
+ *
+ * @author User
+ */
+public class SwitchDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // demonstrate the switch
+        
+        int i;
+        
+        for(i=0; i<10; i++)
+            switch(i) {
+                case 0:
+                    System.out.println("i is zero");
+                    break;
+                case 1:
+                    System.out.println("i is one");
+                    break;
+                case 2:
+                    System.out.println("i is two");
+                    break;       
+                case 3:
+                    System.out.println("i is three");
+                    break;
+                default: 
+                    System.out.println("i is five or more");
+            }
+    }
+    
+}
