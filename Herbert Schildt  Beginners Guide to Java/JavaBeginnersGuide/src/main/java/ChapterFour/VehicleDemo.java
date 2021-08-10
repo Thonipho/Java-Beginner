c543a26b9ba4747f44945db1f71fa5596758a1ba
Thonipho <thonipho@icloud.com>
2021-08-10 21:37:50 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterFour/VehicleDemo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterFour;
+
+/**
+ *
+ * @author User
+ */
+public class VehicleDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // this class declares objects of type vehicle
+        
+        Vehicle minivan = new Vehicle();
+        int range;
+        
+        // assign values to fields in minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+        
+        // compute the range assuming a full tank of gas
+        range = minivan.fuelcap * minivan.mpg;
+        System.out.println("Minivan can carry " + minivan.passengers + "with a range of " + range);
+    }
+    
+}
