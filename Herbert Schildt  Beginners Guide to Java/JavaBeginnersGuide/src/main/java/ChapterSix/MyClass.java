1632a582a69ba0eed7073cf42bfa4721af2704b4
Thonipho <thonipho@icloud.com>
2021-08-16 22:04:23 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterSix/MyClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterSix;
+
+/**
+ *
+ * @author User
+ */
+public class MyClass {
+    private int alpha; //private access
+    public int beta; //public acess
+    int gamma; //default access
+    
+    /* Methods to access alpha. It is ok for a member of a class to access a private 
+    member of the same class.
+    */
+    void setAlpha(int a ) {
+        alpha = a;
+    }
+    int getAlpha() {
+        return alpha;
+    }
+}
