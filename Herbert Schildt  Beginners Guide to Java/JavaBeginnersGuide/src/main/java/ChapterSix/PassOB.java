a42bf06ff746a11de63d8d6d51473b56672a2af2
Thonipho <thonipho@icloud.com>
2021-08-16 22:35:08 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterSix/PassOB.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterSix;
+
+/**
+ *
+ * @author User
+ */
+public class PassOB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Block ob1 = new Block(10,2,5);
+        Block ob2 = new Block(10,2,5);
+        Block ob3 = new Block(4,3,6);
+        
+        System.out.println("ob1 same dimensions as ob2: " + ob1.sameBlock(ob2));
+        System.out.println("ob1 same dimensions as ob3: " + ob1.sameBlock(ob3));
+        System.out.println("ob1 same dimensions as ob3: " + ob1.sameVolume(ob3));
+    }
+    
+}
