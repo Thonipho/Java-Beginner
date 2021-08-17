3a5cb7f747d42916ce54267914b0102633198b6e
Thonipho <thonipho@icloud.com>
2021-08-17 22:39:40 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterSix/Factorial.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterSix;
+
+/**
+ *
+ * @author User
+ */
+public class Factorial {
+    //A simple example of recursion
+    //This is a recursive function
+    
+    int factR(int n) {
+        int result;
+        
+        if(n==1) return 1;
+        result = factR(n-1) * n;
+        return result;
+    }
+    
+    //This is an iterative equivalent
+    int factI(int n) {
+        int t, result;
+        
+        result = 1;
+        for(t=1; t<=n; t++) result +=t;
+        return result;
+    }
+}
