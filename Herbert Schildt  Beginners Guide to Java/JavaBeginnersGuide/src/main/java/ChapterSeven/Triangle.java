a726d3c1088b38c191fa871476ea5045b88157dd
Thonipho <thonipho@icloud.com>
2021-08-18 11:40:05 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterSeven/Triangle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterSeven;
+
+/**
+ *
+ * @author User
+ */
+public class Triangle extends TwoDShape {
+    // a subclass of TwoDShape for triangles
+    
+    String style;
+    
+    double area() {
+        return getWidth()*getHeight()/2;
+    }
+    
+    void showStyle() {
+        System.out.println("Triangle is " + style);
+    }
+}
---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/ChapterSeven/TwoDShape.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterSeven;
+
+/**
+ *
+ * @author User
+ */
+public class TwoDShape {
+    // A simple class hierarchy
+    // A class for two dimensional objects
+    
+    private double width;
+    private double height;
+    
+    //Accessor methods for width and height
+    double getWidth() {return width;}
+    double getHeight() {return height;}
+    void setWidth(double w) {width =w;}
+    void setHeight(double h) {height = h;}
+    
+    void showDim() {
+        System.out.println("Width and height are " + width + " and " + height);
+    }
+}
