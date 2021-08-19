bdee9782248ff56dd4c2925925438bbe97708c3a
Thonipho <thonipho@icloud.com>
2021-08-19 13:43:00 +0200

    Code College

---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/PackagesAndInterfaces/Series.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackagesAndInterfaces;
+
+/**
+ *
+ * @author User
+ */
+public interface Series {
+    int getNext(); //return next number series
+    void reset(); //restart
+    void setStart(int x); //set starting value
+}
+
+
---
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/PackagesAndInterfaces/SeriesDemo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackagesAndInterfaces;
+
+/**
+ *
+ * @author User
+ */
+public class SeriesDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ByTwos ob = new ByTwos();
+        
+        for(int i=0; i<5; i++)
+            System.out.println("Next value is " + ob.getNext());
+        
+        System.out.println("\nResetting");
+        ob.reset();
+        for(int i=0; i<5; i++)
+            System.out.println("Next value is " + ob.getNext());
+        
+        System.out.println("\nStarting at 100");
+        ob.setStart(100);
+        for(int i=0; i<5; i++)
+            System.out.println("Next value is " + ob.getNext());
+    }
+    
+}
