169e9e952dd3d9baac163531f296713c0465f355
Thonipho <thonipho@icloud.com>
2021-08-23 22:47:58 +0200

    Code College

--- Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/UsingIO/CopyFile.java
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/UsingIO/CopyFile.java
@@ -18,21 +18,17 @@ public class CopyFile {
     public static void main(String[] args) throws IOException {
         
         int i;
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
         
-        // First make sure that both files has been specified
+        // first confirm that both files have been specified
         if(args.length != 2) {
             System.out.println("Usage: CopyFile from to");
             return;
         }
         
-        //copy a file
-        try {
-            //attempt to open the files
-            fin = new FileInputStream(args[0]);
-            fout = new FileOutputStream(args[1]);
-            
+        // open and manage two files via the try statement
+        try (FileInputStream fin = new FileInputStream(args[0]);
+            FileOutputStream fout = new FileOutputStream(args[1]))
+        {
             do {
                 i = fin.read();
                 if(i != -1) fout.write(i);
@@ -40,17 +36,6 @@ public class CopyFile {
             
         } catch(IOException exc) {
             System.out.println("I/O Error: " + exc);
-        } finally {
-            try {
-                if(fin != null) fin.close();
-            } catch(IOException exc) {
-                System.out.println("Error closing input file");
-            }
-            try {
-                if(fout != null) fout.close();
-            } catch (IOException exc) {
-                System.out.println("Error closing output file");
-            }
         }
     }
     
