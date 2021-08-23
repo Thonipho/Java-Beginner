6e7521117b1ebea1202b8edcf18d236f8db012c1
Thonipho <thonipho@icloud.com>
2021-08-23 22:39:57 +0200

    Code College

--- Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/UsingIO/ShowFile.java
+++ Herbert Schildt  Beginners Guide to Java/JavaBeginnersGuide/src/main/java/UsingIO/ShowFile.java
@@ -17,41 +17,27 @@ public class ShowFile {
      * @param args the command line arguments
      */
     // Display a text file
+    // Using try-with resources
     public static void main(String[] args) {
 
-        int i;
-        FileInputStream fin = null;
-
-        //First make sure that a file has been specified
-        if (args.length != 1) {
-            System.out.println("Usage: ShowFile File");
-            return;
-        }
-
-        // the following code opens a file, reads characters until EOF is encountered 
-        // then closes a file via a finally block
-        try {
-            fin = new FileInputStream(args[0]);
-
-            do {
-                i = fin.read();
-                if (i != -1) {
-                    System.out.print((char) i);
-                }
-            } while (i != -1);
-            
-        } catch (FileNotFoundException exc) {
-            System.out.println("File not found");
-            return;
-        } catch (IOException exc) {
-            System.out.println("Error reading file");
-        } finally {
-            // close file in all cases
-            try {
-                if(fin != null) fin.close();
-            } catch (IOException exc) {
-                System.out.println("Error closing file");
-            }
-        }
+       int i;
+       
+       // first make sure that a file name has been specified
+       if(args.length != 1) {
+           System.out.println("Usage: ShowFile filename");
+           return;
+       }
+       
+       // the following code uses try-with-resources to open a file and automatically close it
+       try(FileInputStream fin = new FileInputStream(args[0])) {
+           
+           do{
+               i = fin.read();
+               if(i != -1) System.out.print((char) i);
+           } while(i != -1);
+           
+       } catch(IOException exc) {
+           System.out.println("I/O Error: " + exc);
+       }
     }
 }
